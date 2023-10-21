169bcf1d0dc7be149678674868c5fa02c597862a
Fritz Ammon <fritz.ammon@slickdeals.net>
2023-10-21 04:31:54 -0700

    Initial Commit

---
+++ SDSwiftUICoordinator/SDSwiftUICoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDSwiftUICoordinatorApp.swift
+//  SDSwiftUICoordinator
+//
+//  Created by Fritz Ammon on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct SDSwiftUICoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
